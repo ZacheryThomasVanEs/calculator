45bbe88c7feb0fbe57deb504c1daa86c12c4bf82
Zachery Van Es <zachvanes@gmail.com>
2024-07-22 10:35:58 -0700

    First commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def calc(x):
+    try:
+        print(str(eval(x,{"__builtins__":None},{})))
+    except:
+        print("Error")
+
+while True:
+    times = input("Input: ")
+    calc(times)
